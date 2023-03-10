a9972e8cf8ce6d7a56f009aad758f05e7f515281
Víctor Ariza <20313041+arizart@users.noreply.github.com>
2023-03-09 21:18:23 -0600

    Validación de entrada para números.

--- UtilHttpRequest.java
+++ UtilHttpRequest.java
@@ -5,32 +5,40 @@ import java.net.HttpURLConnection;
 import java.net.URL;
 
 public class UtilHttpRequest {
+
+	private URL url;
+	private String response;
+	private HttpURLConnection connection;
+
 	public String GET(String getUrl) throws IOException {
-		URL url = new URL(getUrl);
-		String response = "";
-		HttpURLConnection con = (HttpURLConnection) url.openConnection();
 
-		con.setRequestMethod("GET");
-		con.setRequestProperty("Content-Type", "application/json");
-		con.setConnectTimeout(5000);
-		con.setReadTimeout(5000);
+		url = new URL(getUrl);
+		response = "";
+		connection = (HttpURLConnection) url.openConnection();
 
-		int statusCode = con.getResponseCode();
+		connection.setRequestMethod("GET");
+		connection.setRequestProperty("Content-Type", "application/json");
+		connection.setConnectTimeout(5000);
+		connection.setReadTimeout(5000);
+
+		int statusCode = connection.getResponseCode();
 
 		if (statusCode == HttpURLConnection.HTTP_OK) {
-			BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
-			String inputLine;
+			BufferedReader stream = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+			String currentLine;
 			StringBuffer buffer = new StringBuffer();
 
-			while ((inputLine = in.readLine()) != null) {
-				buffer.append(inputLine);
+			while ((currentLine = stream.readLine()) != null) {
+				buffer.append(currentLine);
 			}
-			in.close();
 
+			stream.close();
 			response = buffer.toString();
+
 		} else {
 			System.out.println("Something went wrong. HTTP Response Code :: " + statusCode);
 		}
+
 		return response;
 	}
 }
---
+++ UtilInputValidation.java
@@ -0,0 +1,39 @@
+import java.awt.event.*;
+
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+
+public class UtilInputValidation implements KeyListener {
+
+	private JTextField inputField;
+	private JLabel result;
+
+	public UtilInputValidation(JTextField input, JLabel outcome) {
+		this.inputField = input;
+		this.result = outcome;
+	}
+
+	@Override
+	public void keyPressed(KeyEvent e) {
+	}
+
+	@Override
+	public void keyReleased(KeyEvent e) {
+
+		char key = e.getKeyChar();
+		boolean validInput = '0' <= key && key <= '9' || key == '.' || key == '\b';
+
+		if (validInput) {
+			result.setText("");
+		} else if (key == '\n') {
+			// TODO: Call convert function.
+		} else {
+			inputField.setText("");
+			result.setText("El campo sólo acepta números.");
+		}
+	}
+
+	@Override
+	public void keyTyped(KeyEvent e) {
+	}
+}
